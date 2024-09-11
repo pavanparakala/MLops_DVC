e9f90075c2b53fc8b4310f0a906932540885ece2
pavan parakala <pavanchandparakala@gmail.com>
2024-09-11 17:36:51 +0530

    deployment

--- MLOps_DVC/main.py
+++ MLOps_DVC/main.py
@@ -3,7 +3,7 @@ from src.CnnClassifier.pipeline.stage_01_data_ingestion import (
     DataIngestionTrainingPipeline,
 )
 from src.CnnClassifier.pipeline.stage_02_prepare_base_model import (
-    PrepareBaseModelTrainingPipline,
+    PrepareBaseModelTrainingPipeline,
 )
 from src.CnnClassifier.pipeline.stage_03_training import (
     ModelTrainingPipeline,
@@ -26,7 +26,7 @@ STAGE_NAME = " Prepare base model"
 try:
     logger.info(f"*******************")
     logger.info(f">>>>>> stage {STAGE_NAME} started<<<<<<<<")
-    obj = PrepareBaseModelTrainingPipline()
+    obj = PrepareBaseModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx==========x")
 except Exception as e:
--- MLOps_DVC/src/CnnClassifier/pipeline/stage_02_prepare_base_model.py
+++ MLOps_DVC/src/CnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -6,7 +6,7 @@ from src.CnnClassifier.utils import logger
 STAGE_NAME = "Prepare base model"
 
 
-class PrepareBaseModelTrainingPipline:
+class PrepareBaseModelTrainingPipeline:
     def __init__(self):
         pass
 
@@ -18,15 +18,13 @@ class PrepareBaseModelTrainingPipline:
         prepare_base_model.update_base_model()
 
 
-if __name__ == "__manin__":
+if __name__ == "__main__":
     try:
         logger.info(f"*******************")
-        logger.info(f">>>>>> stage {STAGE_NAME} started<<<<<<<<")
-        obj = PrepareBaseModelTrainingPipline()
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
         obj.main()
-        logger.info(
-            f">>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx==========x"
-        )
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
         raise e
